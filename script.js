f9cf6d91531d127aae828cf924e0776a952cb847
art3008 <art.osv@mail.ru>
2022-11-04 22:07:13 +0600

    70%

--- script.js
+++ script.js
@@ -4,18 +4,50 @@ const header    = document.getElementById('header')
 const allNotes  = document.getElementById('all_notes')
 const container = document.getElementById('container')
 const body      = document.querySelector('body')
+const hide      = document.getElementById('hide')
 
 
-function addNote () {
 
+function openModal () {
     header.style.backgroundColor = 'rgba(0, 0, 0, 0.5)'
-    allNotes.style.background = 'rgba(0, 0, 0, 0.5)'
-    //body.style.backgroundColor = 'rgba(0, 0, 0, 0.5)'
     modal.style.display = 'block'
+}
 
-    console.log(body);
+function hideModal() {
 
+    header.style.backgroundColor = '#d9b38d'
+    modal.style.display = 'none'
 }
 
 
-addBtn.addEventListener('click', addNote)
+addBtn.addEventListener('click', openModal)
+hide.addEventListener('click', hideModal)
+
+modal.addEventListener('keydown', (event) => {
+    if(event.key == 'Enter') {
+        addNote()
+    }
+})
+
+
+function addNote() {
+
+
+    const note      = document.createElement('div')
+    const details   = document.createElement('div')
+    const descrip   = document.createElement('h2')
+
+    note.className      = 'note'
+    details.className   = 'details'
+
+    
+
+    details.appendChild(descrip.innerHTML)
+    note.appendChild(note.innerHTML)
+    allNotes.appendChild(note)
+    
+
+
+
+
+}
