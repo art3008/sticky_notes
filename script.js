16bc93d4a6fbdaf6eaad3cd03699c2a5098f740f
art3008 <art.osv@mail.ru>
2022-11-04 16:33:02 +0600

    123

--- script.js
+++ script.js
@@ -2,13 +2,18 @@ const addBtn    = document.getElementById('btnAdd')
 const modal     = document.getElementById('modal')
 const header    = document.getElementById('header')
 const allNotes  = document.getElementById('all_notes')
+const container = document.getElementById('container')
+const body      = document.querySelector('body')
+
 
 function addNote () {
 
     header.style.backgroundColor = 'rgba(0, 0, 0, 0.5)'
-    allNotes.style.backgroundColor = 'rgba(0, 0, 0, 0.5)'
+    allNotes.style.background = 'rgba(0, 0, 0, 0.5)'
+    //body.style.backgroundColor = 'rgba(0, 0, 0, 0.5)'
     modal.style.display = 'block'
 
+    console.log(body);
 
 }
 
