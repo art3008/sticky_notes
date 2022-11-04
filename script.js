13f1f0f069a9ef3b88322e8c24e3132ca0c17678
art3008 <art.osv@mail.ru>
2022-11-04 23:02:56 +0600

    100%

--- script.js
+++ script.js
@@ -5,6 +5,18 @@ const allNotes  = document.getElementById('all_notes')
 const container = document.getElementById('container')
 const body      = document.querySelector('body')
 const hide      = document.getElementById('hide')
+const usrInp    = document.getElementById('user_input')
+
+
+let colorsArray = ["#7E6ED4", 
+"#2d7e95", 
+"#5294e2", 
+"#d65bdc",
+"#ff2000",
+"#162fff",
+"#2b80cc"]
+
+
 
 
 
@@ -33,21 +45,24 @@ modal.addEventListener('keydown', (event) => {
 function addNote() {
 
 
-    const note      = document.createElement('div')
-    const details   = document.createElement('div')
-    const descrip   = document.createElement('h2')
+    let randomColor = colorsArray[Math.floor(Math.random() * colorsArray.length)]
+
+    let note      = document.createElement('div')
+    let details   = document.createElement('div')
+    let descrip   = document.createElement('h2')
 
     note.className      = 'note'
+    note.style.backgroundColor = randomColor
+    note.style.transform = `rotate(${Math.floor(Math.random() * -5)}deg)`
     details.className   = 'details'
+    descrip.innerHTML   =  usrInp.value
 
-    
-
-    details.appendChild(descrip.innerHTML)
-    note.appendChild(note.innerHTML)
     allNotes.appendChild(note)
+    note.appendChild(details)
+    details.appendChild(descrip)
     
-
-
-
+    note.addEventListener('click', () => {
+        note.remove()
+    })
 
 }
